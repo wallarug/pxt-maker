475cb658fe25b0d27e8de90a3e75ab8e7ce8e045
wallarug <wallarug@coliemore.com.au>
2020-01-21 22:16:03 +1100

    modified some robohatmm1-m4 board files.  issues with non standard names

--- libs/robohatmm1-m4/config.ts
+++ libs/robohatmm1-m4/config.ts
@@ -1,15 +1,22 @@
+
 namespace config {
-    //Pi Serial
+    export const NUM_NEOPIXELS = 10;
+    export const PIN_NEOPIXEL = DAL.PB23;
+
+    export const PIN_LED = DAL.PB22;
+
+    export const PIN_FLASH_MISO = DAL.PA14;
+    export const PIN_FLASH_MOSI = DAL.PA12;
+    export const PIN_FLASH_SCK = DAL.PB13;
+    export const PIN_FLASH_CS = DAL.PB15;
+
     export const PIN_PI_TX = DAL.PA16;
     export const PIN_PI_RX = DAL.PA17;
-    //NEOPIXEL pin
-    export const PIN_NEOPIXEL = DAL.PB23;
-    export const NUM_NEOPIXEL = 10;
 
-    export const RCC_1 = DAL.PA07;
-    export const RCC_2 = DAL.PA06;
-    export const RCC_3 = DAL.PA05;
-    export const RCC_4 = DAL.PA04;
+    export const PIN_RCC1 = DAL.PA07;
+    export const PIN_RCC2 = DAL.PA06;
+    export const PIN_RCC3 = DAL.PA05;
+    export const PIN_RCC4 = DAL.PA04;
 
     export const PIN_SERVO1 = DAL.PA18;
     export const PIN_SERVO2 = DAL.PA19;
@@ -20,28 +27,20 @@ namespace config {
     export const PIN_SERVO7 = DAL.PA09;
     export const PIN_SERVO8 = DAL.PA08;
 
-    export const PIN_GROVE0 = PIN_SERVO8;
-    export const PIN_GROVE1 = PIN_SERVO7;
-    //I2C_PORT
     export const PIN_SDA = DAL.PA00;
     export const PIN_SCL = DAL.PA01;
 
-    //GPS_PORT
     export const PIN_TX = DAL.PB02;
     export const PIN_RX = DAL.PB03;
-    export const PIN_GPS_SDA = PIN_SDA;
-    export const PIN_GPS_SCL = PIN_SCL;
-    //SPI_PORT
+
     export const PIN_MOSI = DAL.PB08;
     export const PIN_MISO = DAL.PB11;
     export const PIN_SCK = DAL.PB09;
     export const PIN_SS = DAL.PB10
 
-    export const PIN_LED = DAL.PB22;
-    //Flash pins not implement yet
-    // export const PIN_FLASH_MISO = DAL.PA14;
-    // export const PIN_FLASH_MOSI = DAL.PA12;
-    // export const PIN_FLASH_SCK = DAL.PB13;
-    // export const PIN_FLASH_SS = DAL.PB15;
+    export const PIN_GROVE0 = PIN_SERVO8;
+    export const PIN_GROVE1 = PIN_SERVO7;
 
+    export const PIN_GPS_SDA = PIN_SDA;
+    export const PIN_GPS_SCL = PIN_SCL;
 }
--- libs/robohatmm1-m4/device.d.ts
+++ libs/robohatmm1-m4/device.d.ts
@@ -1,51 +1,31 @@
 declare namespace pins {
-    
-    //% fixedInstance shim=pxt::getPin(PIN_D0)
-    const D0: DigitalInOutPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D1)
-    const D1: DigitalInOutPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D2)
-    const D2: DigitalInOutPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D3)
-    const D3: DigitalInOutPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D4)
-    const D4: DigitalInOutPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D5)
-    const D5: DigitalInOutPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D6)
-    const D6: DigitalInOutPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D7)
-    const D7: DigitalInOutPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D8)
-    const D8: DigitalInOutPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D9)
-    const D9: DigitalInOutPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D10)
-    const D10: DigitalInOutPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D11)
-    const D11: DigitalInOutPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D12)
-    const D12: DigitalInOutPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D13)
-    const D13: DigitalInOutPin;
 
-    //% fixedInstance shim=pxt::getPin(PIN_A0)
-    const A0: PwmOnlyPin;
-    //% fixedInstance shim=pxt::getPin(PIN_A1)
-    const A1: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_A2)
-    const A2: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_A3)
-    const A3: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_A4)
-    const A4: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_A5)
-    const A5: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SERVO1)
+    const SERVO1: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SERVO2)
+    const SERVO2: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SERVO3)
+    const SERVO3: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SERVO4)
+    const SERVO4: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SERVO5)
+    const SERVO5: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SERVO6)
+    const SERVO6: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SERVO7)
+    const SERVO7: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SERVO8)
+    const SERVO8: PwmPin;
 
     //% fixedInstance shim=pxt::getPin(PIN_SDA)
     const SDA: DigitalInOutPin;
     //% fixedInstance shim=pxt::getPin(PIN_SCL)
     const SCL: DigitalInOutPin;
+
+    //% fixedInstance shim=pxt::getPin(PIN_GPS_SDA)
+    const GPS_SDA: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_GPS_SCL)
+    const GPS_SCL: DigitalInOutPin;
 
     //% fixedInstance shim=pxt::getPin(PIN_MISO)
     const MISO: DigitalInOutPin;
@@ -53,6 +33,10 @@ declare namespace pins {
     const MOSI: DigitalInOutPin;
     //% fixedInstance shim=pxt::getPin(PIN_SCK)
     const SCK: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_SS)
+    const SS: DigitalInOutPin;
+    
+    
 }
 
 declare namespace input {
